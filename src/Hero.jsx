42eff81ccb4bd9bd86bf1d510e93f167e2a8640b
kartikpadal <kartikpadal17@gmail.com>
2025-07-13 18:40:14 +0530

    starting portfolio from today

---
+++ src/Hero.jsx
@@ -0,0 +1,18 @@
+
+import './index.css';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <div className="hero-image"></div>
+        <div className="hero-text">
+          <h1>Kartik Padal</h1>
+          <p>Welcome to my portfolio website!</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
