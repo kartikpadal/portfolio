98859d728c3496ef787afcdfa76c6c49e4f99dbe
kartikpadal <kartikpadal17@gmail.com>
2025-11-06 23:46:24 +0530

    making minip cards

--- src/minip.jsx
+++ src/minip.jsx
@@ -1,14 +1,25 @@
+function Minip() {
+  
 
+  return (
+    <>
+      <section className="minip-section">
+        <h2 className="mini-title">Mini-Projects</h2>
 
-function Minip(){
-    return(
-        <>
-        <div>
-            <h>Mini-projects</h>
+        <div className="mini-scroll-container">
+          {projects.map((p, i) => (
+            <div className="mini-card" key={i}>
+              <h3>{p.title}</h3>
+              <p>{p.desc}</p>
+              <a href={p.link} target="_blank" className="mini-btn">
+                View Project →
+              </a>
+            </div>
+          ))}
         </div>
-        </>
-    );
-
+      </section>
+    </>
+  );
 }
 
 export default Minip;
